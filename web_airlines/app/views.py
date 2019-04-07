f467d7b6e72d8c11f85b24044a46ba2f15fed7b8
vs42 <32885206+vs42@users.noreply.github.com>
2019-04-07 14:25:00 +0300

    Update views.py

--- web_airlines/app/views.py
+++ web_airlines/app/views.py
@@ -59,7 +59,9 @@ def login():
     f = open('text.txt', 'r')
     for line in f:
         if (line.split()[0] == request.post[username] and line.split()[1] == request.post[password]):
-            return render_template('mainpage.html')        
+            redirect('mainpage.html')
+            return render_template('mainpage.html')
+    redirect('loginpage.html')
     return render_template('loginpage.html')
 
 
@@ -67,4 +69,5 @@ def login():
 def register():
     f = open('text.txt', 'a')
     f.write(request.post[username] + ' ' + request.post[password] + ' ' + request.post[name] + '\n')
-    return render_template('mainpage.html')
+    redirect('mainpage.html')
+    return render_template('startpage.html')
